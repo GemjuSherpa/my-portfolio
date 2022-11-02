1130f8f35e4b0a120e56f32f72a1a4fdee2415e7
Gemju Sherpa <gemju.s1@gmail.com>
2022-11-02 14:47:02 +1100

    Basic header and styling added

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logo from "../../images/logo.png";
+
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+
+      {/* nav links */}
+      <div className="nav-items">
+        <ul className="nav-list">
+          <Link to="/">
+            <li className="active">Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/experience">
+            <li>Experiences</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
+
+        {/* resume button */}
+        <a href="#" className="resume">Resume</a>
+      </div>
+    </div>
   )
 }
 
