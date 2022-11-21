d1b80a0505602cc65df4e3e1065c11f1b569a599
Gemju Sherpa <gemju.s1@gmail.com>
2022-11-21 16:01:20 +1100

    Footer component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <div >
+        About
+      </div>
     )
   }
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './Footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div>Footer</div>
-    )
-  }
+export default function Footer() {
+  return (
+    <footer className="footer">
+
+      {/* Social media links */}
+      <div className="social">
+        <ul>
+          <li>
+            <a href="https://twitter.com/Gemju_sherpa" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} className="social-icon" />
+            </a>
+          </li>
+          <li> 
+            <a href="https://www.facebook.com/gemju.sherpa/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+            </a>
+          </li>
+          <li> 
+            <a href="https://github.com/GemjuSherpa" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="social-icon" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/gemjusherpa/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="social-icon"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Copyright tag */}
+      <div className="copyright">
+        Copyright &copy; {new Date().getFullYear()} Gemju Sherpa
+      </div>
+      
+    </footer>
+  )
 }
+
