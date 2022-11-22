d345bdf2955ade7993acc4dd310622561547fa82
Gemju Sherpa <gemju.s1@gmail.com>
2022-11-22 14:45:44 +1100

    Experiences component added

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Moment from "react-moment";
+import { useSelector } from 'react-redux';
+import {getAllProfiles} from '../../features/profile/profileSlice';
 
-export default class Experience extends Component {
-  render() {
-    return (
-      <div>Experience</div>
-    )
-  }
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+
+import './Experience.scss';
+
+export default function Experience() {
+
+  const profile = useSelector(getAllProfiles);
+
+  const experiences = profile.experience;
+
+  const workExperiences = experiences?.map((experience) => (
+    <div className="card" key={experience.id}>
+      <div className="card-header">
+        <h3>{experience.role} @ <a href={experience.url} target="_blank" rel="noopener noreferrer">{experience.company}</a></h3>
+        <h4><Moment format="YYYY">{experience.from}</Moment> -
+          {experience.to === "" ? (
+            "Now"
+          ) : (
+            <Moment format="YYYY">{experience.to}</Moment>
+          )}
+        </h4>
+      </div>
+      <div className="card-body">
+        {experience.responsibilities?.map((item)=>(
+          <ul>
+            <li><FontAwesomeIcon icon={faCaretRight} className="carret-icon"></FontAwesomeIcon>{item}</li>
+          </ul>
+        ))}
+      </div>
+      <div className="card-footer">
+        <h5>Skills utilised:</h5>
+        <p>{experience.skills}</p>
+      </div>
+    </div>
+  ));
+
+
+  return (
+    <div className="experiences">
+      <h1>My professional experiences</h1><hr />
+      {workExperiences}
+    </div>
+  )
 }
